4af84ee5afd1e6b67274021f85063b69bcf72de1
mehrdadanvar <mehrdad.anvar@gmail.com>
2023-11-19 06:55:36 -0800

    corrected the fonts

--- app.vue
+++ app.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="app">
     <NuxtLayout>
-      <div>
-        <NuxtLoadingIndicator />
-        <NuxtPage />
-      </div>
+      <NuxtPage />
     </NuxtLayout>
   </div>
 </template>
@@ -30,17 +27,18 @@ async function report() {
   background-color: red;
 }
 @font-face {
-  font-family: "Encode Sans";
+  font-family: "Open Sans";
   font-style: normal;
   font-weight: 400;
   font-stretch: 100%;
   font-display: swap;
-  src: url(https://fonts.gstatic.com/s/encodesans/v19/LDIcapOFNxEwR-Bd1O9uYNmnUQomAgE25imKSbHhROjLsZBWTSrQGGHjVtKF7Q.woff2)
+  src: url(https://fonts.gstatic.com/s/opensans/v36/memSYaGs126MiZpBA-UvWbX2vVnXBbObj2OVZyOOSr4dVJWUgsjZ0B4gaVI.woff2)
     format("woff2");
-  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329,
-    U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
+    U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191,
+    U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
 }
 .app {
-  font-family: "Encode Sans";
+  font-family: "Open Sans";
 }
 </style>
