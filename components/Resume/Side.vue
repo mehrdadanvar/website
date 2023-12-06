02a816ba60a5f8570bfc77009dc793b570ae7e92
mehrdadanvar <mehrdad.anvar@gmail.com>
2023-12-06 04:53:34 -0800

    fixed socials on large and small screens

--- components/Resume/Side.vue
+++ components/Resume/Side.vue
@@ -9,17 +9,21 @@
     </div>
 
     <p class="text-md lg:text-xl text-gray-500">
-      I build application that facilitate care, teach programming and statistics.
+      I build application that facilitate care, teach programming and
+      statistics.
     </p>
     <address
-      class="flex container mx-auto gap-4 transition-all duration-75 ease-linear lg:gap-10 lg:justify-center"
+      class="flex container mx-auto gap-2 md:gap-4 transition-all duration-75 ease-linear lg:flex-wrap lg:gap-10 lg:justify-center"
     >
       <div
         v-for="item in socials"
         :key="item.id"
         class="sm:text-md lg:text-2xl text-gray-500 transition-all ease-linear duration-75 hover:text-cyan-600 hover:scale-110"
       >
-        <a :href="item.link.includes('https') ? item.link : `${item.link}`" target="_blank">
+        <a
+          :href="item.link.includes('https') ? item.link : `${item.link}`"
+          target="_blank"
+        >
           <UIcon :name="item.icon_name" />
         </a>
       </div>
@@ -34,9 +38,21 @@ let side_props = defineProps({
 });
 console.log(side_props);
 let socials = [
-  { id: 0, icon_name: "i-simple-icons-linkedin", link: "https://www.linkedin.com/in/mehrdad-anvar/" },
-  { id: 1, icon_name: "i-simple-icons-github", link: "https://github.com/mehrdadanvar" },
-  { id: 2, icon_name: "i-simple-icons-gmail", link: "mailto:mehrdad.anvar@gamil.com" },
+  {
+    id: 0,
+    icon_name: "i-simple-icons-linkedin",
+    link: "https://www.linkedin.com/in/mehrdad-anvar/",
+  },
+  {
+    id: 1,
+    icon_name: "i-simple-icons-github",
+    link: "https://github.com/mehrdadanvar",
+  },
+  {
+    id: 2,
+    icon_name: "i-simple-icons-gmail",
+    link: "mailto:mehrdad.anvar@gamil.com",
+  },
   { id: 3, icon_name: "i-simple-icons-gitlab", link: "" },
   { id: 4, icon_name: "i-carbon-phone", link: "tel:+12368665644" },
 ];
